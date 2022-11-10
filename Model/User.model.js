5f2c9be3067e65dd33314e7b2015553bd9e91d96
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2022-11-10 19:28:45 +0530

    Backend Data

---
+++ Model/User.model.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+
+// const UserSchema= mongoose.Schema({
+//     name:{type:String, required:true},
+//     email:{type:String,required:true},
+//     password:{type:String,required:true}
+// })
+
+const UserSchema= mongoose.Schema({
+        number:{type:Number,required:true}
+    })
+
+
+const UserModel=mongoose.model("user",UserSchema)
+
+module.exports={UserModel}
