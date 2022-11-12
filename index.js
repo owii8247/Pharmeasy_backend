9085a006fd0e9656a3817ab76b776f104a515ae7
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2022-11-12 12:54:38 +0530

    Updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors=require("cors")
 const bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
 const {CartRouter}=require("./Routes/Cart.route")
-const PORT = process.env.PORT || 8080;
+
 const app = express();
 app.use(cors())
 app.use(express.json());
@@ -22,14 +22,13 @@ app.get("/", (req, res) => {
 
 app.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
-  const data = await UserModel.find({ email });
-  console.log(data);
+  const data = await UserModel.findOne({ email });
   if (data) {
-    res.send({ "msg": "User Already Exhist" });
+    res.send({ "msg": "User Already Exhist",toast:"i" });
   } else {
     bcrypt.hash(password, 5, async function (err, hashed_password) {
       if (err) {
-        res.send({ "msg": "Please Signup again" });
+        res.send({ "msg": "Please Signup again",toast:"e" });
       } else {
         const new_user = UserModel({
           name,
@@ -37,59 +36,59 @@ app.post("/signup", async (req, res) => {
           password: hashed_password,
         });
         await new_user.save();
-        res.send({ "msg": "Signup Sucessfull" });
+        res.send({ "msg": "Signup Sucessfull" ,toast:"s"});
       }
     });
   }
 });
 
-// ***** LOGIN ROUTE***** //
-
-// app.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-//   const user = await UserModel.findOne({ email });
-//   if (!user) {
-//     res.send({ "msg": "Please Login first" });
-//   } else {
-//     const hashed_password = user.password;
-//     const user_id = user._id;
-//     bcrypt.compare(password, hashed_password, function (err, result) {
-//       if (err) {
-//         res.send({ "msg": "Please Login Again" });
-//       }
-//       if (result) {
-//         var token = jwt.sign({ user_id }, process.env.SECRET_KEY);
-//         res.send({ "msg": "Login SucessFull", "token": token });
-//       } else {
-//         res.send({ "msg": "wrong Credential" });
-//       }
-//     });
-//   }
-// });
-
-
-// **** LOGIN BY NUMBER ************ 
+//***** LOGIN ROUTE***** //
 
 app.post("/login", async (req, res) => {
-  const { number } = req.body;
-  console.log(number)
-  const user = await UserModel.findOne({ number });
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email });
   if (!user) {
-      const new_user = UserModel({
-        number,
-      });
-      await new_user.save();
-      const otp = Math.floor(1000 + Math.random() * 9000);
-      res.send({ "msg": "Signup Sucessfull" ,"otp":otp,"user":new_user});
+    res.send({ "msg": "Please Signup first",toast:"i" });
   } else {
-    const otp = Math.floor(1000 + Math.random() * 9000);
-    res.send({ "msg": "Signup Sucessfull" ,"otp":otp,"user":user});
+    const hashed_password = user.password;
+    const user_id = user._id;
+    bcrypt.compare(password, hashed_password, function (err, result) {
+      if (err) {
+        res.send({ "msg": "Please Login Again",toast:"e" });
+      }
+      if (result) {
+        var token = jwt.sign({ user_id }, process.env.SECRET_KEY);
+        res.send({ "msg": "Login SucessFull", "token": token,toast:"s" });
+      } else {
+        res.send({ "msg": "wrong Credential",toast:"e" });
+      }
+    });
   }
 });
 
 
+// **** LOGIN BY NUMBER ****** 
+
+// app.post("/login", async (req, res) => {
+//   const { number } = req.body;
+//   console.log(number)
+//   const user = await UserModel.findOne({ number });
+//   if (!user) {
+//       const new_user = UserModel({
+//         number,
+//       });
+//       await new_user.save();
+//       const otp = Math.floor(1000 + Math.random() * 9000);
+//       res.send({ "msg": "Signup Sucessfull" ,"otp":otp,"user":new_user});
+//   } else {
+//     const otp = Math.floor(1000 + Math.random() * 9000);
+//     res.send({ "msg": "Signup Sucessfull" ,"otp":otp,"user":user});
+//   }
+// });
 
 
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, async () => {
   try {
     await connection;
@@ -98,5 +97,4 @@ app.listen(PORT, async () => {
     console.log("Error to connectiong DB-server");
     console.log(err);
   }
-  console.log(`Listening on PORT ${PORT}`);
 });
